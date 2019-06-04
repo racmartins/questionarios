18f3c346336236072fb0e8fad63428e71b26a6f9
Rui Martins <racmartins@netcabo.pt>
2019-06-04 19:50:13 +0100

    Parte 2

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                 <div class="card-body">
                    @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ str_plural('voto', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ str_plural('resposta', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . str_plural('visualizações', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
